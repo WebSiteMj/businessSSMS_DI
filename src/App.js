f7cccfeac717bf738659075d773e126b1d698d16
Madiha <websiteMj@hotmail.com>
2025-10-24 22:18:49 +0200

    Full project commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+// import logo from './logo.svg';
+import logo from "./assets/logo.svg";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        "Innovating Systems, Inspiring Minds."<code>src/App.js</code> and save to reload.
+        </p>
+      
+         I’m excited to share the journey of SSMS_DI, where we turn ideas into scalable solutions and nurture the next generation of IT professionals.
+With expertise in .NET, Blazor, SQL, fintech, and automation, we deliver products that streamline processes, improve efficiency, and empower our clients.
+Our recent projects include:
+• Fintech solutions for seamless payments and registrations
+• Private stock exchange platform for cryptocurrency clients
+• Custom software solutions designed for business growth
+Alongside building solutions, I’m passionate about teaching Azure and mentoring IT students, helping those hungry for knowledge gain practical skills and confidence in the tech world.
+At SSMS_DI, we value innovation, quality, and strong client relationships, and we’re always open to collaborating with visionary partners, investors, and clients.
+Slogan: "Innovating Systems, Inspiring Minds."
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/README.md
@@ -0,0 +1,1 @@
+# businessSSMS_DI
